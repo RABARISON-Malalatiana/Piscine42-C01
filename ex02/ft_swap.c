90f58fe7c7a6d3805fe9bc078152ccafc91a9e99
Malalatiana Rabarison <marabari@student.42.fr>
2023-09-10 12:13:00 +0200

    c01

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: marabari <marabari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 19:38:47 by marabari          #+#    #+#             */
-/*   Updated: 2023/09/07 20:35:46 by marabari         ###   ########.fr       */
+/*   Updated: 2023/09/10 12:11:49 by marabari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,20 @@ void	ft_swap(int *a, int *b)
 {
 	int	temp;
 
-	tempi = *a;
+	temp = *a;
 	*a = *b;
 	*b = temp;
 }
+
+/*int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 12;
+	b = 56;
+	printf("avant a est %d et b est %d\n", a, b);
+	ft_swap(&a, &b);
+	printf("apres a est %d et b est %d", a, b);
+	return (0);
+}*/
